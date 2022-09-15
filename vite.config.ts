c81178f65a876af561674cb30a46f53e7ab5fe93
sunsonghao <songhaosun@gmail.com>
2022-09-15 23:37:22 +0800

    feat: 项目初始化，husky安装deng

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: sunsh
+ * @Date: 2022-09-15 16:10:10
+ * @LastEditors: sunsh
+ * @LastEditTime: 2022-09-15 22:20:00
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]
+})
