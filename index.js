dce32ff6d949059477b078d1d969f578d37f9f40
sunsonghao <songhaosun@gmail.com>
2022-09-27 12:14:40 +0800

    feat: 副作用注册函数，改善依赖存储结构

--- index.js
+++ index.js
@@ -3,32 +3,57 @@
  * @Author: sunsh
  * @Date: 2022-09-27 10:48:01
  * @LastEditors: sunsh
- * @LastEditTime: 2022-09-27 11:19:20
+ * @LastEditTime: 2022-09-27 12:11:59
  */
 let origin = {
   foo: 'foo'
 }
 
-function effect() {
-  document.getElementById("app").innerText = origin.foo
+// 用于注册副作用的函数, 解决副作用函数名字硬编码
+let activeEffect;
+function effect(cb) {
+  activeEffect = cb;
+  cb();
 }
 
-const bucket = new Set();
+// 依赖集合的数据结构
+const bucket = new WeakMap(); //[[origin, [originKey, new Set([handle1, ···])]], ···]
 
 origin = new Proxy(origin, {
   get(target, key, receiver) {
-    
-    bucket.add(effect); // effect名字硬编码，不具有通用性
+    // bucket.add(activeEffect); // 解决硬编码问题
+
+    // 依赖收集
+    if (!activeEffect) return;
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+      bucket.set(target, (depsMap = new Map()));
+    }
+
+    let deps = depsMap.get(key);
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
 
     return Reflect.get(target, key, receiver);
   },
   set(target, key, newValue, receiver) {
+    Reflect.set(target, key, newValue, receiver); // 先设置新值
     
-    bucket.forEach(fn => fn()); // 不能根据key区分对应的依赖，下次提交改进
+    bucket.get(target)?.get(key)?.forEach(fn => {
+      fn();
+    });
     
-    Reflect.set(target, key, newValue, receiver);
     return true;
   }
 });
 
-effect();
+function handler1() {
+  document.getElementById("app").innerText = origin.foo
+}
+effect(handler1);
+
+setTimeout(() => {
+  origin.foo = 'reactive'
+}, 1000);
